32493946a9a5f275066e5f0843d135a91f091b53
KARAOUI <jamal.karaoui@capgemini.com>
2018-12-04 17:45:55 +0100

    first commit

---
+++ src/main/java/com/example/demo/ServerGestionclientApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerGestionclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerGestionclientApplication.class, args);
+	}
+}
